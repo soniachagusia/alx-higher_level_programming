0d806c57309c2c8d85f41a83d5ee2d0b4ee710ca
Sonia Chagusia <Navalayochagusia@gmail.com>
2024-03-12 09:38:26 -0700

    0x0D-SQL_introduction

---
+++ 7-insert_value.sql
@@ -0,0 +1,2 @@
+-- Script to insert three new rows into the table first_table
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
---
+++ 8-count_89.sql
@@ -0,0 +1,2 @@
+-- Script to display the number of records with id = 89 in the table firsttable
+SELECT COUNT(*) FROM first_table WHERE id = 89;
